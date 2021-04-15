2dffdf2fca23572fed704b60442018b7d3f75606
Patricia Polts <patriciapoltts@gmail.com>
2021-04-15 03:34:14 -0300

    add front home layout

---
+++ public/js/home.js
@@ -0,0 +1,10 @@
+$(function () {
+    /**
+     * Tab menu
+     */
+    // var firstTabEl = document.querySelector('#navmenu li:last-child a');
+    // var firstTab = new bootstrap.Tab(firstTabEl);
+  
+    // firstTab.show();
+
+  });

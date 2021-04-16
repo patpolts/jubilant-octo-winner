a6775f9decfe47a57352ac29f5af8aebfc22efa9
Patricia Polts <patriciapoltts@gmail.com>
2021-04-16 01:08:32 -0300

    update layou

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 
-<div class="container-fluid">
-    <header>
+<div class="container-fluid {{!$isHome ? 'single' : '' }}">
+    <header >
         <div class="row">
             <div class="col-12">
                 <!-- content header -->
